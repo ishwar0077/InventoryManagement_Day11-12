f868154580faf22c359d32676fbfafea66b1a09b
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-16 11:54:12 +0530

    Simple Inventory Management

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace InventoryManagement
+{
+    public class FetchForJsonInventoryData
+    {
+        public Inventory Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Inventory>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+    }
+}
